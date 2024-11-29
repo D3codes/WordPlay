55670b15974ac137f867d856977a3409dc3a55af
David Freeman <david@d3.codes>
2024-11-29 00:09:08 -0600

    Initial Commit

---
+++ Minute CrypticTests/Minute_CrypticTests.swift
@@ -0,0 +1,16 @@
+//
+//  Minute_CrypticTests.swift
+//  Minute CrypticTests
+//
+//  Created by David Freeman on 11/29/24.
+//
+
+import Testing
+
+struct Minute_CrypticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Minute CrypticUITests/Minute_CrypticUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Minute_CrypticUITestsLaunchTests.swift
+//  Minute CrypticUITests
+//
+//  Created by David Freeman on 11/29/24.
+//
+
+import XCTest
+
+final class Minute_CrypticUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
